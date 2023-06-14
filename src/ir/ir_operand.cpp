f63b1420e7ffbc85267b03d903f4821eba2938b5
XXL <1981349981@qq.com>
2023-06-14 21:40:56 +0800

    为operand重载“<”

--- src/ir/ir_operand.cpp
+++ src/ir/ir_operand.cpp
@@ -3,5 +3,9 @@
 #include <string>
 #include <utility>
 
+ir::Operand::Operand(std::string n, Type t) : name(std::move(n)), type(t) {}
 
-ir::Operand::Operand(std::string  n, Type t): name(std::move(n)), type(t) {}
+bool operator<(Operand &that) const
+{
+    return this.name < that.name;
+}
